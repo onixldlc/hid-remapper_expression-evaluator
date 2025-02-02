d0d89e2d4902072ba47786c34090432ea2454ccd
onixldlc <onxdl16427@outlook.com>
2025-02-02 17:03:56 +0700

    add: added utility to check object

---
+++ js/utils.js
@@ -0,0 +1,31 @@
+function isEmptyObject(value) {
+    if (value == null) {
+      // null or undefined
+      return false;
+    }
+  
+    if (typeof value !== 'object') {
+      // boolean, number, string, function, etc.
+      return false;
+    }
+  
+    const proto = Object.getPrototypeOf(value);
+  
+    // consider `Object.create(null)`, commonly used as a safe map
+    // before `Map` support, an empty object as well as `{}`
+    if (proto !== null && proto !== Object.prototype) {
+      return false;
+    }
+  
+    return isEmpty(value);
+}
+
+function isEmpty(obj) {
+    for (const prop in obj) {
+        if (Object.hasOwn(obj, prop)) {
+        return false;
+        }
+    }
+
+    return true;
+}
